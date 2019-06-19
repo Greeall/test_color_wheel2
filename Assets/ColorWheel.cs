7eaadf5088e2782ea4d3dc07a6fe807aa558e701
Greeall <greeall@yandex.ru>
2019-06-19 18:27:23 +0300

    Initial commit

---
+++ Assets/ColorWheel.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ColorWheel : MonoBehaviour, IPointerClickHandler, IDragHandler
+{
+    public MainUI mainUI;
+    public GameObject tooltip;
+    public Camera camera;
+    public Canvas canvas;
+    Color[] Data;
+    Image Image;
+
+    public int Width { get { return Image.sprite.texture.width; } }
+    public int Height { get { return Image.sprite.texture.height; } }
+
+    void Awake()
+    {
+        Image = GetComponent<Image>();
+        Data = Image.sprite.texture.GetPixels();
+    }
+
+    public void HideTooltip()
+    {
+        tooltip.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData eventData) 
+    {
+        OnColorSelect(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        OnColorSelect(eventData);
+    }
+
+    void OnColorSelect(PointerEventData eventData)
+    {
+        Color color = GetWheelColor(eventData.position);
+        if (color.a > 0.95f)
+        {
+            tooltip.SetActive(true);
+            tooltip.transform.position = eventData.position;
+            mainUI.CurrentColor = color;
+            tooltip.GetComponent<Image>().color = color;
+        }
+    }
+
+    Color GetWheelColor(Vector2 position)
+    {
+        RectTransform rt = GetComponent<RectTransform>();
+        float wheelX = transform.position.x - ((rt.rect.width * canvas.scaleFactor) / 2);
+        float wheelY = transform.position.y - ((rt.rect.height * canvas.scaleFactor) / 2);
+        float xRelativePos = position.x - wheelX;
+        float yRelativePos = position.y - wheelY;
+        float resizeFactor = rt.rect.width * canvas.scaleFactor / Width;
+        Debug.Log(resizeFactor);
+        return Data[(int)(yRelativePos / resizeFactor) * Width + (int)(xRelativePos / resizeFactor)];
+    }
+}
---
+++ Assets/MainUI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class MainUI : MonoBehaviour {
+    public string imageUrl;
+    public GameObject imagePrefab;
+    public GameObject newImageWindow;
+    public Button confirmButton;
+    public ScrollRect list;
+
+    Texture2D currentImage;
+    Color _currentColor;
+    public Color CurrentColor
+    {
+        get { return _currentColor; }
+        set { _currentColor = value; }
+    }
+
+    public void OpenNewImageWindow()
+    {
+        newImageWindow.SetActive(true);
+        StartCoroutine(DownloadImage());
+    }
+
+    public void CloseNewImageWindow()
+    {
+        newImageWindow.SetActive(false);
+        // Reset Image
+        confirmButton.interactable = false;
+        currentImage = null;
+    }
+
+    public void AddNewImage()
+    {
+        // Create Image
+        GameObject newImage = Instantiate<GameObject>(imagePrefab);
+        newImage.transform.parent = list.content.transform;
+        // Assign Sprite
+        Sprite sprite = Sprite.Create(currentImage, new Rect(0, 0, currentImage.width, currentImage.height), new Vector2(0.5f, 0.5f));
+        Image imgCmp = newImage.GetComponent<Image>();
+        imgCmp.sprite = sprite;
+        imgCmp.color = CurrentColor;
+        // Scroll list to the bottom where newest image is
+        Canvas.ForceUpdateCanvases();
+        list.verticalNormalizedPosition = 0;
+        // Close window
+        CloseNewImageWindow();
+    }
+
+    IEnumerator DownloadImage()
+    {
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(imageUrl))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                currentImage = DownloadHandlerTexture.GetContent(uwr);
+                confirmButton.interactable = true;
+            }
+        }
+    }
+}
